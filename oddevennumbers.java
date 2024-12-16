40e232367820cba8ef22616b219080a342c6d00e
rohithreddy <rohiththummala183@gmail.com>
2024-12-16 15:25:16 +0530

    java Docker test

---
+++ oddevennumbers.java
@@ -0,0 +1,12 @@
+public class oddevennumbers {
+    public static void main(String[] args) {
+        int number = 29; // Change this number to test different values
+        
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}
+
